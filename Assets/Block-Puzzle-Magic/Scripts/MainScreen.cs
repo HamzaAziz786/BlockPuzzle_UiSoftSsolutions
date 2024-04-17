82ddf6c98d25c2d417b83ce44fedbaf60d5cfdb2
HamzaAziz786 <98694278+HamzaAziz786@users.noreply.github.com>
2024-04-17 10:07:24 +0500

    settingsdone

--- Assets/Block-Puzzle-Magic/Scripts/MainScreen.cs
+++ Assets/Block-Puzzle-Magic/Scripts/MainScreen.cs
@@ -2,15 +2,20 @@
 using System.Collections;
 
 public class MainScreen : MonoBehaviour 
-{	
-	/// <summary>
-	/// Raises the play button pressed event.
-	/// </summary>
-	public void OnPlayButtonPressed()
+{
+    /// <summary>
+    /// Raises the play button pressed event.
+    /// </summary>
+    private void Start()
+    {
+        AdsController.instance.ShowAd(AdType.BANNER , 1);
+    }
+    public void OnPlayButtonPressed()
 	{
 		if (InputManager.Instance.canInput ()) {
 			AudioManager.Instance.PlayButtonClickSound ();
 			StackManager.Instance.selectModeScreen.Activate();
 		}
 	}
+
 }
